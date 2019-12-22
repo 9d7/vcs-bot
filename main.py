bd8549d54f6151ccb63f9e512887897cce685cbd
ericeschnei <ericeschnei@gmail.com>
2019-12-22 15:07:33 -0500

    Doin' some Heroku nonsense

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from discord.ext import commands
 from cogs.help import HelpCog
-
+import os
 def main():
   bot = commands.Bot(command_prefix="!")
   bot.remove_command('help')
 
   bot.add_cog(HelpCog('help.yaml', bot))
 
+  bot.run(os.environ['DISCORD_KEY'])
 
 
 if __name__ == "__main__":
