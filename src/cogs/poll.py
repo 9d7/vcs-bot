dee1b10b3b3aad64b7ab4a98a6aecb629f5bb7ca
ericeschnei <ericeschnei@gmail.com>
2019-12-25 02:20:44 -0500

    More work on poll. Added a bit more in the YAML

--- src/cogs/poll.py
+++ src/cogs/poll.py
@@ -2,9 +2,6 @@ from discord.ext import commands
 import discord
 import psycopg2
 from ruamel.yaml import YAML
-import datetime
-import box
-from contextlib import suppress
 from src.base import *
 
 class PollCog(commands.Cog):
@@ -24,6 +21,36 @@ class PollCog(commands.Cog):
     if ctx.invoked_subcommand is None:
       await send(ctx, self.messages['command-not-found'], tag=True, expire=True)
 
+  @poll.command()
+  async def create(self, ctx: commands.context, *args):
+    if len(args) == 0:
+      await send(ctx, self.messages['wrong-arg-length'], tag=True, expire=True)
+      return
+    elif len(args) == 1:
+      await send(ctx, "aaaaa", tag=True, expire=True)
+    else:
+      await send(ctx, "bbbbb", tag=True, expire=True)
+
+
+  @poll.command()
+  async def append(self, ctx: commands.context, *args):
+    pass
+
+  @poll.command()
+  async def list(self, ctx: commands.context, *args):
+    pass
+
+  @poll.command()
+  async def revive(self, ctx: commands.context, *args):
+    pass
+
+  @poll.command()
+  async def view(self, ctx: commands.context, *args):
+    pass
+
+  @poll.command()
+  async def purge(self, ctx: commands.context, *args):
+    pass
 
 
 
