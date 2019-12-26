1b023079c20c04604fd5430798cfd61e2ae2ebf5
ericeschnei <ericeschnei@gmail.com>
2019-12-25 19:55:42 -0500

    Some more poll work. Trying to get create working

--- src/cogs/poll.py
+++ src/cogs/poll.py
@@ -3,17 +3,22 @@ import discord
 import psycopg2
 from ruamel.yaml import YAML
 from src.base import *
+from datetime import datetime
+
 
 class PollCog(commands.Cog):
-
-  def snowflake_to_str(self, flake: int):
-    return "{0:016X}".format(flake)
 
   def __init__(self, poll_file: str, cursor, bot: commands.Bot):
     self.bot = bot
     self.cursor = cursor
     with open(poll_file, 'r') as msg_file:
       self.messages = YAML(typ='safe').load(msg_file)
+
+  def get_poll_string(self, poll_id):
+    return "Testing. Will work shortly!"
+
+  def snowflake_to_str(self, flake: int):
+    return "{0:016X}".format(flake)
 
   @commands.group()
   @delete_source
@@ -28,11 +33,43 @@ class PollCog(commands.Cog):
       await send(ctx, self.messages['errors']['wrong-arg-length'],
                  tag=True, expire=True)
       return
-    elif len(args) == 1:
-      await send(ctx, "aaaaa", tag=True, expire=True)
-    else:
-      await send(ctx, "bbbbb", tag=True, expire=True)
 
+    message = await send(ctx, self.messages['messages']['loading'],
+                         tag=False, expire=False)
+
+    message_flake = self.snowflake_to_str(message.id)
+    channel_flake = self.snowflake_to_str(ctx.channel.id)
+    user_flake = self.snowflake_to_str(ctx.author.id)
+    question = args[0]
+
+    self.cursor.execute("INSERT INTO Polls "
+                        "(Question, Username, Time, "
+                        "LastUpdate, Message, Channel) "
+                        "VALUES (%s, %s, %s, %s, %s, %s) RETURNING PollID;",
+                        (question, user_flake, datetime.now(),
+                         datetime.now(), message_flake, channel_flake))
+
+    poll_id = self.cursor.fetchone()[0]
+
+    if len(args) == 1:
+      options = zip(self.messages['default_poll']['options'],
+                    self.messages['default_poll']['emojis'])
+    else:
+      options = zip(args[1:],
+                    self.messages['emojis'])
+
+    for option in options:
+      self.cursor.execute("INSERT INTO Options "
+                          "(PollID, Original, Username, Emoji, Option) "
+                          "VALUES (%s, %s, %s, %s, %s);",
+                          (poll_id, True, user_flake, option[1], option[0]))
+
+    message_string = self.get_poll_string(poll_id)
+
+
+
+    header = self.messages['style']['poll-header'].format(0, ctx.author.name)
+    q_str = self.messages['style']['question-string'].format(question)
 
   @poll.command()
   async def append(self, ctx: commands.context, *args):
@@ -53,8 +90,3 @@ class PollCog(commands.Cog):
   @poll.command()
   async def purge(self, ctx: commands.context, *args):
     pass
-
-
-
-
-
